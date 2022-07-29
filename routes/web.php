278f8a2f576bf591db69021f83585bde2fe62df9
Marea Norton <nortonmarea@gmail.com>
2022-07-30 08:42:09 +0930

    Finish chapter 8+

--- routes/web.php
+++ routes/web.php
@@ -9,22 +9,48 @@ use App\Http\Controllers\PostsController;
 
 
 /*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
+    GET -Request a resource
+    POST - Create a new resource
+    PUT - Update a resource
+    PATCH - Modify a resource
+    DELETE - Delete a resource
+    OPTIONS - Ask the server which verbs are allowed
 */
 
-//Route::get('/blog', [PostsController::class, 'index']);
+// GET
+Route::get('/blog', [PostsController::class, 'index']);
+Route::get('/blog/1', [PostsController::class, 'show']);
 
-Route::resource('blog', PostsController::class);
+
+//POST
+Route::get('/blog/create', [PostsController::class, 'create']);
+Route::post('/blog', [PostsController::class, 'store']);
+
+// PUT OR PATCH
+Route::get('/blog/edit/1', [PostsController::class, 'edit']);
+Route::patch('/blog/1', [PostsController::class, 'update']);
+
+//DELETE
+Route::delete('/blog/1', [PostsController::class, 'destroy']);
+
+//Route::resource('blog', PostsController::class);
 
 // Route for invoke method
 Route::get('/', HomeController::class);
+
+
+// Multiple HTTP verbs
+//Route::match(['GET', 'POST'], '/blog', [PostsController::class, 'index']);
+//Route::any('/blog', [PostsController::class, 'index']);
+
+// Return view
+//Route::view('/blog', 'blog.index');
+
+
+
+
+
+
 
 
 
